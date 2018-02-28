2f0a8652a68adfda6ca29753b1f5a17f1cbe128f
ZBK3\Jordan <jordansanogo@gmail.com>
2018-02-28 11:23:45 +0100

    Modifications mineurs

---
+++ GestionDeTicketsSolution/GestionDeTickets/TicketScreenUser.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace GestionDeTickets
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class TicketScreenUser
+    {
+        public TicketScreenUser()
+        {
+            InitializeComponent();
+        }
+    }
+}
